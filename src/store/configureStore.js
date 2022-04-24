467a95cc25d9c59ab49ab04299361dbb2f6f6811
sagar <sagarcg1234@gmail.com>
2022-04-24 22:05:46 +0530

    store updated

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,16 +4,31 @@ import moviesReduces from '../reducers/moviesReducer'
 
 
 export const saveToLocalStorage = (state) => {
-    const serialisedState = JSON.stringify(state);
-    console.log(serialisedState)
-    localStorage.setItem("persistantState", serialisedState);
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
 }
+
+
+// export const saveToLocalStorage = (state) => {
+//     const serialisedState = JSON.stringify(state);
+//     console.log(serialisedState)
+//     localStorage.setItem("persistantState", serialisedState);
+// }
 
 // load string from localStarage and convert into an Object
 export const loadFromLocalStorage = () => {
-    const serialisedState = localStorage.getItem("persistantState") || []
-    console.log(serialisedState)
-    return JSON.parse(serialisedState);
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
 }
 
 const configureStore = () => {
